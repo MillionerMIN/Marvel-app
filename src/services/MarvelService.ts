af66083199c44e0f3380d8b82eb5d515566c8bc4
Vladimir Liankevich <vladimirlyn@gmail.com>
2021-11-20 14:16:54 +0300

    install packes of package.json, prettier, husky, lint-stagesd

--- src/services/MarvelService.ts
+++ src/services/MarvelService.ts
@@ -5,54 +5,56 @@ export class MarvelService {
     const res = await fetch(url);
 
     if (!res.ok) {
-      return new Error(`Could not fetch ${url}, status: ${res.status}`)
+      return new Error(`Could not fetch ${url}, status: ${res.status}`);
     }
 
-    return await res.json()
-  }
+    return await res.json();
+  };
 
   getAllCharacters = async () => {
-    const res = await this.getResource(`${this._baseUrl}characters?limit=9&offset=210&${this._apiKey}`)
+    const res = await this.getResource(
+      `${this._baseUrl}characters?limit=9&offset=210&${this._apiKey}`
+    );
     return res.data.results.map(this._transformCharacter);
-  }
+  };
   getCharacter = async (id: number) => {
-    const res = await this.getResource(`${this._baseUrl}characters/${id}?${this._apiKey}`)
-    return this._transformCharacter(res.data.results[0])
-  }
+    const res = await this.getResource(
+      `${this._baseUrl}characters/${id}?${this._apiKey}`
+    );
+    return this._transformCharacter(res.data.results[0]);
+  };
   _transformCharacter = (char: CharacterApiType): CharacterType => {
     return {
       name: char.name,
       description: char.description,
-      thumbnail:
-        char.thumbnail.path +
-        '.' +
-        char.thumbnail.extension,
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
       homepage: char.urls[0].url,
       wiki: char.urls[1].url,
-    }
-  }
-
+    };
+  };
 }
 
 export type CharacterType = {
-  name: null | string
-  description: null | string
-  thumbnail: null | string
-  homepage: null | string
-  wiki: null | string
-}
+  name: null | string;
+  description: null | string;
+  thumbnail: null | string;
+  homepage: null | string;
+  wiki: null | string;
+};
 
 type CharacterApiType = {
-  name: null | string
-  description: null | string
+  name: null | string;
+  description: null | string;
   thumbnail: {
-    path: null | string
-    extension: null | string
-  }
-  urls: [{
-    url: null | string;
-  },
+    path: null | string;
+    extension: null | string;
+  };
+  urls: [
     {
       url: null | string;
-    }]
-}
+    },
+    {
+      url: null | string;
+    }
+  ];
+};
