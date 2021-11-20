a4772d84735d36954de604321130c2b5d235bffb
Vladimir Liankevich <vladimirlyn@gmail.com>
2021-11-20 15:21:41 +0300

    added component spinner

---
+++ src/components/common/spinner/Spinner.tsx
@@ -0,0 +1,10 @@
+export const Spinner = () => {
+  return (
+    <div className="spinner">
+      <div className="blob top"></div>
+      <div className="blob bottom"></div>
+      <div className="blob left"></div>
+      <div className="blob move-blob"></div>
+    </div>
+  );
+};
