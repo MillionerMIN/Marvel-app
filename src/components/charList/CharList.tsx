2cc80296f69f1d87400dbe8e39e1448a79089e01
Vladimir Liankevich <vladimirlyn@gmail.com>
2021-11-22 14:27:54 +0300

    modify commponent CharList, added onCharactersLoadde, componnentDid Mount, messagerError and Spiner

--- src/components/charList/CharList.tsx
+++ src/components/charList/CharList.tsx
@@ -1,52 +1,61 @@
 import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import { Component } from 'react';
+import { CharacterType, MarvelService } from '../../services/MarvelService';
+import { MessageError } from '../common/error/MessageError';
+import { Spinner } from '../common/spinner/Spinner';
 
-const CharList = () => {
-  return (
-    <div className="char__list">
-      <ul className="char__grid">
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
+class CharList extends Component {
+  state = {
+    characters: [] as CharacterType[],
+    loading: true,
+    error: false,
+  };
+
+  marvelService = new MarvelService();
+
+  onCharactersLoaded = (characters: CharacterType[]) => {
+    this.setState({
+      characters,
+      loading: false,
+    });
+  };
+
+  componentDidMount = () => {
+    this.marvelService.getAllCharacters().then(this.onCharactersLoaded);
+    console.log('####: charList mount');
+  };
+
+  componentWillUnmount = () => {
+    console.log('####: charList unmount');
+  };
+
+  render() {
+    console.log('####: charList render');
+    const { characters, loading, error } = this.state;
+    const messageError = error ? <MessageError /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content =
+      !(error || loading) &&
+      characters.map((item) => (
+        <li className="char__item" key={item.name}>
+          <img src={item.thumbnail} alt="abyss" />
+          <div className="char__name">{item.name}</div>
         </li>
-        <li className="char__item char__item_selected">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-      </ul>
-      <button className="button button__main button__long">
-        <div className="inner">load more</div>
-      </button>
-    </div>
-  );
-};
+      ));
+
+    return (
+      <div className="char__list">
+        <ul className="char__grid">
+          {messageError}
+          {spinner}
+          {content}
+        </ul>
+        <button className="button button__main button__long">
+          <div className="inner">load more</div>
+        </button>
+      </div>
+    );
+  }
+}
 
 export default CharList;
